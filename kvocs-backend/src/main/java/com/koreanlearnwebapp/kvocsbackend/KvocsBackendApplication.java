294c5979a5486017cd3a2b33a3820a3c11bc3609
SViktorS <viktor.schimpf90@web.de>
2023-04-30 20:59:21 +0200

    add: initial projects for frontend and backend

---
+++ kvocs-backend/src/main/java/com/koreanlearnwebapp/kvocsbackend/KvocsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.koreanlearnwebapp.kvocsbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KvocsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KvocsBackendApplication.class, args);
+	}
+
+}
---
+++ kvocs-backend/src/test/java/com/koreanlearnwebapp/kvocsbackend/KvocsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.koreanlearnwebapp.kvocsbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KvocsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
